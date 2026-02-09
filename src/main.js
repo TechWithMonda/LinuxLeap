94f411a63d177457e6f9eb5353c23483ab065f4e
TechWithMonda <mondadevops@gmail.com>
2026-02-09 06:40:05 +0300

    vercel

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import './assets/main.css'
+import { Analytics } from "@vercel/analytics/next"
 
-createApp(App).mount('#app')
+createApp(App).use(Analytics).mount('#app')
